3e9971a7a491fd44f530669393979ff05d2fcd1f
Maria Fernanda Rincon <fernandarincon1930@gmail.com>
2025-02-02 12:49:43 -0500

    Actualizar app

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+function agregarAmigo(){
+    nombreIngresado= document.getElementById('amigo');
+    if (nombreIngresado==""){
+        alert('Porfavor inserte un nombre');
+        agregarAmigo();
+    }
+    amigos.push(nombreIngresado);
+    document.getElementById('amigo')
+}
+function sortearAmigo(){
+
+}
